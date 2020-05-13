5af6699ffc6d1ab56572626cdb96fe886a643cc6
juraj <cold.slayer@hotmail.com>
2020-05-13 12:07:24 +0200

    remove unnecessary storybook additions

--- template/.storybook/config.tsx
+++ template/.storybook/config.tsx
@@ -1,14 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { addDecorator, configure } from "@storybook/react";
-import "@abraham/reflection";
 import { RouterDecorator } from "../.storybook/decorators/RouterDecorator";
-
-import moment from "moment";
-import "moment/locale/hr";
-import { Locale } from "enums/Locale";
-import { AppConfigProviderDecorator } from "../.storybook/decorators/AppConfigProviderDecorator";
-import { DiProviderDecorator } from "../.storybook/decorators/DiProviderDecorator";
-moment.locale(Locale.Croatian);
 
 import requireContext from "require-context.macro";
 
@@ -20,13 +11,8 @@ function loadStories() {
 
 configure(loadStories, module);
 
-import "../src/root/App.module.scss";
+import "../src/App.css";
 
 // Don't add global decorator for store because in certain stories store has default value!
 // use stories/decorators/StoreProviderDecorator.tsx for decorating stories that need redux
 addDecorator(RouterDecorator);
-addDecorator(AppConfigProviderDecorator);
-addDecorator(DiProviderDecorator);
-
-// ⚠️ For some reason, rendering global AppRootDivDecorator.tsx does not render children inside it, but adjacent to it!
-// Therefore, it's required to add the decorator manually in each story for the global styles ⚠️
--- template/.storybook/decorators/DiProviderDecorator.tsx
+++
@@ -1,9 +0,0 @@
-import { DependencyInjectionProvider } from "providers/DependencyInjectionProvider/DependencyInjectionProvider";
-import * as React from "react";
-
-// tslint:disable-next-line:no-any
-export const DiProviderDecorator = (storyFn: any) => {
-    return (
-        <DependencyInjectionProvider>{storyFn()}</DependencyInjectionProvider>
-    );
-};
